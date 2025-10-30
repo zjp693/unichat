5d60be0f20c59102fda3e5de4bf3d0266b4ac725
AAAAI666 <208275211+AAAAI666@users.noreply.github.com>
2025-10-30 11:57:19 +0800

    refactor(chat): 移除调试日志，优化聊天列表项的显示

--- config/appkit.tsx
+++ config/appkit.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: zhangjianping 942680978@qq.com
- * @Date: 2025-09-03 15:01:55
- * @LastEditors: zhangjianping 942680978@qq.com
- * @LastEditTime: 2025-10-17 23:49:28
- * @FilePath: \unichat\config\appkit.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { cookieStorage, createStorage } from 'wagmi';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import {
@@ -38,7 +30,7 @@ export const wagmiAdapter = new WagmiAdapter({
   ssr: true, // 启用 SSR 支持
   projectId,
   networks, // 传递显式类型化的网络数组
-  pollingInterval: 3_000 // 设置全局轮询间隔为 3 秒
+  pollingInterval: 8_000 // 设置全局轮询间隔为 8 秒
 });
 
 // 导出适配器生成的 Wagmi 配置
